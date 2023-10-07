5203a452e04e380a0950f6bf534e908d728733e2
Alexandr Pakhomov <palex068@gmail.com>
2023-10-08 01:10:58 +0300

    New added

---
+++ README.md
@@ -0,0 +1,11 @@
+# Описание
+
+# Курсы
+
+## [Stepik. Основы языка Си](/StepikC/README.md)
+
+# Материалы
+
+[Основы языка C (learnc.info)](/LearnCinfo/README.md)
+
++ К.Ю. Поляков, А.П. Шестаков, Е.А. Еремин. [Алгоритмизация и программирование](http://kpolyakov.spb.ru/download/inf-2011-01.pdf) // Информатика, № 17, 2011, с. 4-33. [зеркало](https://youngcoder.ru/lessons/2/komp_arifm.pdf), [открыть](/Lib/komp_arifm.pdf)
---
+++ StepikC/003/003.c
@@ -0,0 +1,86 @@
+// #include <stdio.h>
+
+// int main() {
+//     float f = 3.1415926;
+//     int N = 512;
+//     printf("%d\n",N);
+//     printf("%.5d\n",N);
+//     printf("%5.0f\n",f);
+//     printf("%5.2f\n",f);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main() {
+
+//     int x = 3, y = -4;
+//     char ch_eq = '=';
+//     char ch_plus = '+'; 
+//     char ch_minus = '-'; 
+//     char ch_div = '/'; 
+//     char ch_mul = '*';
+//     double res = -0.75;
+
+  
+//     // printf("/d /c /d /c /d\n", x, ch_plus, y, ch_eq, x+y);
+//     // printf(Result of %c for %d %d = %5.2f, ch_div, x, y, res);
+//     // printf("%c %d %d \n%d", x, ch_minus, y, x-y);
+//     printf("%d %c %d %c %d\n", x, ch_plus, y, ch_eq, x+y);
+//     // printf(" %c %d %d \n",ch_minus, x, y, x-y);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main() {
+  
+//     printf("S.Holmes:\n");
+//     printf("51grad 31'25.48\" N\n");
+//     printf("0 grad  9'29.93\" W");
+    
+//   return 0;
+// }
+
+
+// #include <stdio.h> 
+// int main(void){
+//   int a=3, b=4;
+//   double res = 0.75;
+
+//   printf("||-----|-----|-----|-----||\n");
+//   printf("|| act | one | two | res ||\n");
+//   printf("||=====+=====+=====+=====||\n");
+//   printf("||%5c|%-5d|%-5d|%.5d||\n",'+',a,b,a+b);
+//   printf("||%5c|%5d|%5d|%-.4d||\n",'-',a,b,a-b);
+//   printf("||%5c|%5d|%-5d|%.5d||\n",'*',a,b,a*b);
+//   printf("||%-5c|%-5d|%5d|%.3f||\n",'/',a,b,res);
+//   printf("===========================");
+
+//   return(0);
+// }
+
+// #include <stdio.h>
+// int main(void){
+//   printf("\"I'll be back!\"\n\t(c)Terminator");
+//   return 0;
+// }
+
+// #include <stdio.h> 
+// int main(void) {
+
+//   printf("N\t10*N\t100*N\t1000*N\n\n");  
+//   for (int i=1;i<=10;i++)
+//     printf("%d\t%d\t%d\t%d\n",i,10*i,100*i,1000*i);
+
+//   return(0);
+// }
+
+// #include <stdio.h> 
+// int main(void){
+
+//   for (int i=1; i<=5; i++){
+//     for (int j=1; j<=5; j++)
+//       printf("%-5d",i*j);
+//     printf("\n");
+//   }
+
+//   return(0);
+// }
