4364ce59e1c6a32952e9c43a9a4e44236be70cdd
Alexandr Pakhomov <palex068@gmail.com>
2024-04-29 13:21:33 +0300

    9. Движемся от Си к C++

---
+++ Good_good_C_C++/files/09.cpp
@@ -0,0 +1,16 @@
+// Первая программа на языке С++
+// #include <iostream>
+
+// int main() {
+//     std::cout << "Hello World!" << std::endl;
+
+//     return 0;
+// }
+
+#include <iostream>
+
+int main() {
+    std::cout << "I love C++" << std::endl;
+
+    return 0;
+}
